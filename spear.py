085db32ec37749850c7f87c816a93bc2947dddc2
Karan Goel <karan@goel.im>
2014-06-19 07:18:02 -0700

    more formatting and spacing between texts

--- spear.py
+++ spear.py
@@ -17,10 +17,12 @@ def main(num):
   posts = get_today()
   sorted_posts = sorted(posts, key=lambda post: post['rank'])
 
+  click.echo()
   for post in sorted_posts:
-    click.secho('%d - ' % post['rank'], nl=False)
-    click.secho('%s - ' % post['title'], bold=True, fg="red", nl=False)
+    click.secho('%d. ' % post['rank'], nl=False)
+    click.secho('%s\t' % post['title'], bold=True, fg="red", nl=False)
     click.secho('%s' % post['tagline'], fg="yellow")
+    click.echo()
 
 
 if __name__ == '__main__':
