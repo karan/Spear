764cd9f5ec9153e9392d65099ac3a76936e73c3a
Karan Goel <karan@goel.im>
2014-06-18 22:43:07 -0700

    basic version that prints stuff

--- spear.py
+++ spear.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
-def main():
-  print 'Works'
+import click
+import requests
+
+
+TODAY_URL = 'http://hook-api.herokuapp.com/today'
+
+def get_today():
+  return requests.get(TODAY_URL).json()['hunts']
+
+@click.command()
+@click.option('--num', '-n', default=10, help='Number of top products.')
+def main(num):
+  posts = get_today()
+  sorted_posts = sorted(posts, key=lambda post: post['rank'])
+
+  for post in posts:
+    click.echo('%d - %s - %s - %s comments' % (post['rank'], post['title'], post['tagline'], post['comment_count']))
+
 
 if __name__ == '__main__':
   main()
