c54b25f3439fcfa81aa93110c63f0d84fc673135
Karan Goel <karan@goel.im>
2014-06-18 22:10:10 -0700

    add starter code

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Spear',
+      version='0.0.1',
+      description='Product Hunt for Hackers.',
+      author='Karan Goel',
+      license='MIT',
+      keywords = "product hunt product hunt feed rss tool cli",
+      author_email='karan@goel.im',
+      url='http://github.com/karan/Spear',
+      scripts=['spear.py'],
+      entry_points = {
+        'console_scripts': [
+            'hunt = spear:main'
+        ],
+      }
+)
---
+++ spear.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def main():
+  print 'Works'
+
+if __name__ == '__main__':
+  main()
