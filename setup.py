9b627d64bde151fb95026dfe2a3b272f4ed7014b
Karan Goel <karan@goel.im>
2014-06-19 22:27:51 -0700

    bump to v0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name='spear-cli',
-  version='0.0.1',
+  version='0.0.2',
   description='Product Hunt for Hackers.',
   author='Karan Goel',
   license='MIT',
